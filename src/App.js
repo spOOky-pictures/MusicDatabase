7579040e2f60c172816903dece692feea56d7ba2
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-16 20:19:25 +0000

    add js file for the player component and css presets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Fetch from "./Fetch";
 import { useState } from "react";
 
 import SearchBar from "./components/SearchBar/SearchBar";
-import Player from "./components/Player";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -12,7 +11,6 @@ function App() {
       <SearchBar updateSearchQuery={setSearchQuery} />
       <h1>Return random result for "{searchQuery}":</h1>
       {searchQuery && (<div id="apiDiv"><Fetch searchQuery={searchQuery}/></div>)}
-      <Player />
     </div>
   );
 }
