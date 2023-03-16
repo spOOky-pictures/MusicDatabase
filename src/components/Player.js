0e1f257242c921a248c0cfeb20be1d749986828b
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-16 18:33:02 +0000

    delete buttons from the player card

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
 
 function Player(props) {
     return (
@@ -11,26 +9,6 @@ function Player(props) {
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Subtitle>{props.subtitle}</Card.Subtitle>
-                <Button variant="primary" href={props.ghLink} target="_blank">
-                <BsGithub /> &nbsp;
-                {props.isBlog ? "Blog" : "GitHub"}
-                </Button>
-                {"\n"}
-                {"\n"}
-
-                {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-                {!props.isBlog && props.demoLink && (
-                <Button
-                    variant="primary"
-                    href={props.demoLink}
-                    target="_blank"
-                    style={{ marginLeft: "10px" }}
-                >
-                    <CgWebsite /> &nbsp;
-                    {"Demo"}
-                </Button>
-                )}
             </Card.Body>
         </Card>
     )
