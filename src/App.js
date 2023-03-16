8793db624c5e0589ff6d72fb3298aa2bd25ba3e0
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-16 14:46:00 +0000

    add button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Button buttonVariant="text" buttonColor="success" buttonText="Primary" />
     </div>
   );
 }
