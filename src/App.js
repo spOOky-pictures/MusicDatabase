9f6da469462263f37a6fc716e789eb0fb7ce2c3f
NukeTurtle <estiglic@edocuments.co.uk>
2023-03-16 00:55:11 +0000

    create API fetch with basic results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import $ from "jquery";
 import './App.css';
+import Fetch from "./Fetch";
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <h1>Return random result for "eminem":</h1>
+      <div id="apiDiv"><Fetch /></div>
     </div>
   );
 }
