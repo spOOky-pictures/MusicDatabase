72f8edc0cc372fe99223a0687e2bb2dd65cf426b
Elvis Stiglic <41340032+NukeTurtle@users.noreply.github.com>
2023-03-16 20:46:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Fetch from "./Fetch";
 import { useState } from "react";
+import Particle from "./components/Particle";
 
 import SearchBar from "./components/SearchBar/SearchBar";
 
@@ -8,6 +9,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   return (
     <div>
+      <Particle />
       <SearchBar updateSearchQuery={setSearchQuery} />
       <h1>Return random result for "{searchQuery}":</h1>
       {searchQuery && (<div id="apiDiv"><Fetch searchQuery={searchQuery}/></div>)}
