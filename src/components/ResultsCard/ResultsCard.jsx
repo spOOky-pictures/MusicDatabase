ade5f4183d39d77dcd3228d97904c4431c8a4ee9
Davvid96 <zajacek19@gmail.com>
2023-03-17 16:51:12 +0000

    started to make the results card component

---
+++ src/components/ResultsCard/ResultsCard.jsx
@@ -0,0 +1,12 @@
+import "./ResultsCard.css";
+
+const ResultsCard = ({}) => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <h3>Artist Name</h3>
+    </div>
+  );
+};
+
+export default ResultsCard;
