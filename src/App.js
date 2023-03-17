c9970210cc034d7209e5bdcded0634421ddea352
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-17 16:01:11 +0000

    fix bugs regarding the background with particles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Fetch from "./Fetch";
 import { useState } from "react";
+import { Container } from "react-bootstrap";
 import Particle from "./components/Particle";
 
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -9,10 +10,14 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   return (
     <div>
-      <Particle />
-      <SearchBar updateSearchQuery={setSearchQuery} />
-      <h1>Return random result for "{searchQuery}":</h1>
-      {searchQuery && (<div id="apiDiv"><Fetch searchQuery={searchQuery}/></div>)}
+      <Container fluid className="main" id="main">
+        <Particle />
+        <Container className="main-content">
+          <SearchBar updateSearchQuery={setSearchQuery} />
+          <h1>Return random result for "{searchQuery}":</h1>
+          {searchQuery && (<div id="apiDiv"><Fetch searchQuery={searchQuery}/></div>)}
+        </Container>
+      </Container>
     </div>
   );
 }
