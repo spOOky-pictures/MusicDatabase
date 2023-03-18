8df4ade75ba3d297e11ab58fc1818e4b00c5a5ba
Davvid96 <zajacek19@gmail.com>
2023-03-18 00:36:41 +0000

    added artist page routing

--- src/pages/Artist/Artist.jsx
+++ src/pages/Artist/Artist.jsx
@@ -1,12 +1,13 @@
 import "./Artist.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
-
+import { useParams } from "react-router-dom";
 
 const Artist = ({ setSearchQuery }) => {
+  const { id } = useParams();
   return (
     <div>
       <SearchBar updateSearchQuery={setSearchQuery} />
-      <h1>Hello Artist Page</h1>
+      <h1>Hello Artist Page {id} </h1>
     </div>
   );
 };
