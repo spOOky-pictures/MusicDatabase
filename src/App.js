b883208f03d6ca1d0461ea050f38b43629c515fa
Davvid96 <zajacek19@gmail.com>
2023-03-18 00:44:18 +0000

    cleaned up code and removed unsude calls and functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./App.css";
-import Fetch from "./Fetch";
 import { useState } from "react";
-import Particle from "./components/Particle";
 
-import SearchBar from "./components/SearchBar/SearchBar";
-import Grid from "./components/Grid/Grid";
-import ResultsCard from "./components/ResultsCard/ResultsCard";
 import Home from "./pages/Home/Home";
 import Song from "./pages/Song/Song";
 import Artist from "./pages/Artist/Artist";
+
+import Particle from "./components/Particle";
+// import Fetch from "./Fetch";
+
+import "./App.css";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home searchQuery={searchQuery} setSearchQuery={setSearchQuery} />,
+      element: (
+        <Home searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      ),
     },
     {
       path: "/song/:id",
@@ -31,13 +32,6 @@ function App() {
     <div>
       <Particle />
       <RouterProvider router={router} />
-      {/* <h1>Return random result for "{searchQuery}":</h1> */}
-      {/* {searchQuery && (
-        <div id="apiDiv">
-          <Fetch searchQuery={searchQuery} />
-        </div>
-      )} */}
-
     </div>
   );
 }
