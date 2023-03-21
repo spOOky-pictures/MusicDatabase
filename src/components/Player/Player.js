c49be830d06630f3e651f5b6cd61a81b8b25796d
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-21 14:17:46 +0100

    add progress bar js file, some changes in the player component, it adds the progress bar and create some functions to play/pause the song and time update

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -7,41 +7,84 @@ import { VscDebugPause } from "react-icons/vsc";
 import { VscDebugReverseContinue } from "react-icons/vsc";
 import './Player.css';
 import ProgressBar from '../ProgressBar/ProgressBar';
+import ReactAudioPlayer from 'react-audio-player';
 
 class Player extends Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    playing: false
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPlaying: false,
+      currentTime: 0,
+      duration: 0,
+  };
+}
+
+componentDidUpdate(prevProps) {
+  if (prevProps.track !== this.props.track) {
+    this.setState({
+      isPlaying: false,
+      currentTime: 0,
+      duration: 0,
+    });
   }
 }
 
-handlePlayerClick = () => {
-  if (!this.state.playing) {
-    this.setState({playing: true})
-  } else {
-    this.setState({playing: false})
-  }
-}
+handlePlayPause = () => {
+  this.setState({ isPlaying: !this.state.isPlaying });
+};
 
-  render() {
-    return (
-      <Card className='player-card'>
-        <Card.Img className='player-card-img' variant="left" src={this.props.imgPath} alt="player-card-img" />
-        <Card.Body className='player-body'>
-            <div className='text'>
-            <Card.Title className='artist-name'>{this.props.title}</Card.Title>
-            <Card.Subtitle className='song-name'>{this.props.subtitle}</Card.Subtitle>
-            </div>
-            <ProgressBar className='progress' />
-            <Button><VscDebugReverseContinue /></Button>
-            <Button>{this.state.playing? <VscDebugPause onClick={this.handlePlayerClick} /> : <VscDebugStart onClick={this.handlePlayerClick} />}</Button>
-            <Button><VscDebugContinue /></Button>
-        </Card.Body>
-      </Card>
-    )
+handleBackward = () => {
+  // logic to go backward in the track
+};
+
+handleForward = () => {
+  // logic to go forward in the track
+};
+
+handleTimeUpdate = e => {
+  this.setState({
+    currentTime: e.target.currentTime,
+    duration: e.target.duration,
+  });
+};
+
+render() {
+  const { track } = this.props;
+  const { isPlaying, currentTime, duration } = this.state;
+
+  if (!track) {
+    return <div>Loading...</div>; // or any other suitable UI when track is undefined
   }
+
+  return (
+    <Card className='music-player'>
+      <Card.Img className='player-card-img' variant="left" src={track.albumCover} alt={track.songName} />
+      <Card.Body className='player-body'>
+          <div className='text'>
+          <Card.Title className='artist-name'>{track.artistName}</Card.Title>
+          <Card.Subtitle className='song-name'>{track.songName}</Card.Subtitle>
+          </div>
+          <ReactAudioPlayer
+          src={track.audioUrl}
+          onPlay={this.handlePlayPause}
+          onPause={this.handlePlayPause}
+          onEnded={this.handleForward}
+          onTimeUpdate={this.handleTimeUpdate}
+          autoPlay={false}
+          controls={false}
+          />
+          <ProgressBar currentTime={currentTime} duration={duration} />
+          <div>
+          <Button onClick={this.handleBackward}><VscDebugReverseContinue /></Button>
+          <Button onClick={this.handlePlayPause}>{this.state.playing? <VscDebugPause onClick={this.handlePlayerClick} /> : <VscDebugStart onClick={this.handlePlayerClick} />}</Button>
+          <Button onClick={this.handleForward}><VscDebugContinue /></Button>
+          </div>
+      </Card.Body>
+    </Card>
+  )
+}
 }
 
 export default Player;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
-const ProgressBar = ({ progressBarRef }) => {
-    const handleProgressChange = () => {
-        console.log(progressBarRef.current.value);
-    };
+const ProgressBar = ({currentTime, duration}) => {
+  const percentage = (currentTime / duration) * 100;
 
-    return (
-      <div className="progress">
-        <span className="time current">00:00</span>
-        <input 
-        type="range" 
-        ref={progressBarRef}
-        defaultValue='0'
-        onChange={handleProgressChange}
-        />
-        <span className="time">03:34</span>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="progress-bar">
+      <div className="progress" style={{width: `${percentage}%`}} />
+    </div>
+  );
+};
+
 export default ProgressBar;
