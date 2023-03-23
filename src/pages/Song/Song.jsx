0a997dd76c66c809dca95ae19ab5b5242eb877be
Elvis Stiglic <41340032+NukeTurtle@users.noreply.github.com>
2023-03-23 18:10:54 +0000

    Update Song.jsx

--- src/pages/Song/Song.jsx
+++ src/pages/Song/Song.jsx
@@ -11,7 +11,7 @@ const Song = ({ setSearchQuery }) => {
   let filteredSections;
 
   const getSongData = async () => {
-    const RAPID_API_KEY = "b0a488d969msh0a425f0258f176bp14aaefjsn8a4e011bc7d4";
+    const RAPID_API_KEY = "bbfd93fea6mshb8c0e61137028e8p1d542ejsnaaa4222ae123";
     const options = {
       method: "GET",
       headers: {
